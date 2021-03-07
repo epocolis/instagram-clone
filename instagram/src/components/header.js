1763d03758d83821db2ec8df5710ec8491360c0b
leotis <mekdemcrossit@tutanota.com>
2021-03-07 06:25:52 -0500

    styled header

--- instagram/src/components/header.js
+++ instagram/src/components/header.js
@@ -4,6 +4,12 @@ import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 
 export default function Header() {
+  const { firebase } = useContext(FirebaseContext);
+  const user = {
+    displayName: 'karl',
+  };
+
+  //const user = null;
   const markup = (
     <header className="h-16 bg-white border-b mb-8">
       <div className="container mx-auto max-width-lg h-full">
@@ -18,6 +24,55 @@ export default function Header() {
                 />
               </Link>
             </h1>
+          </div>
+          <div className="text-gray text-center flex items-center align-items">
+            {user ? (
+              <>
+                <Link to={ROUTES.DASHBOARD} arial-label="Home">
+                  <p>Dashboard</p>
+                </Link>
+                <button
+                  type="button"
+                  title="Sign Out"
+                  onClick={() => firebase.auth().signOut()}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                      firebase.auth().signOut();
+                    }
+                  }}
+                >
+                  Sign Out
+                </button>
+                <div className="flex items-center cursor-pointer">
+                  <Link to={`/p/${user.displayName}`}>
+                    <img
+                      className="rounded-full h-8 w-8 flex"
+                      src={`/images/avatars/${user.displayName}.jpg`}
+                      alt={`${user.displayName} profile picture`}
+                    />
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <>
+                <Link to={ROUTES.LOGIN}>
+                  <button
+                    type="button"
+                    className="bg-blue-500 font-bold text-sm rounded text-white w-20 h-8"
+                  >
+                    Login
+                  </button>
+                </Link>
+                <Link to={ROUTES.SIGN_UP}>
+                  <button
+                    type="button"
+                    className="font-bold text-sm rounded text-blue w-20 h-8"
+                  >
+                    Sign Up
+                  </button>
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
