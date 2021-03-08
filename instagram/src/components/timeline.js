625e93658a9115f6c6f6984fe0098c5db2cfd133
leotis <mekdemcrossit@tutanota.com>
2021-03-08 14:18:49 -0500

    add picture skeleton

--- instagram/src/components/timeline.js
+++ instagram/src/components/timeline.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import Skeleton from 'react-loading-skeleton';
 
 export default function Timeline() {
-  return <p>I am the timeline</p>;
+  const photos = [1, 2, 3, 4, 5];
+
+  return (
+    <div>
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : (
+        photos.map((content) => <p>I will be a photo!</p>)
+      )}
+    </div>
+  );
 }
