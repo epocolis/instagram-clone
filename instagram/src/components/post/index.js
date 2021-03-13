3fcbc47f791586864827ce00113671a2d4f1b9d8
leotis <mekdemcrossit@tutanota.com>
2021-03-13 06:28:48 -0500

    created post component shell

---
+++ instagram/src/components/post/index.js
@@ -0,0 +1,15 @@
+//import Header from './header';
+
+import React from 'react';
+//import Image from './image';
+//import Actions from './actions';
+//import Footer from './footer';
+//import Comments from './comments';
+
+export default function Post({ content }) {
+  return (
+    <div className="rounded col-span-4 border bg-white mb-16">
+      <p>I am going to be a styled div!</p>
+    </div>
+  );
+}
